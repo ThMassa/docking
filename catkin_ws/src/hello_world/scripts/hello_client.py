e5472579708626ecb107c83232ad6d4548a63f9f
BretzelDev <hugohofmann13@gmail.com>
2023-10-09 13:24:05 +0000

    test publisher subscxriber

--- catkin_ws/src/hello_world/scripts/hello_client.py
+++ catkin_ws/src/hello_world/scripts/hello_client.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
-
+# coding: latin-1
 import rospy
-from hello_world.srv import HelloWorld, HelloWorldRequest
+from std_msgs.msg import String
 
-def hello_client():
-    rospy.wait_for_service('hello_world')
+def callback(data):
+    rospy.loginfo("Message reçu : %s", data.data)
+
+def subscriber_node():
+    # Initialisation du noeud ROS
+    rospy.init_node('mon_subscriber', anonymous=True)
+
+    # Crée un abonnement au topic "mon_topic" de type String
+    rospy.Subscriber('mon_topic', String, callback)
+
+    # Attend que des messages soient reçus
+    rospy.spin()
+
+if __name__ == '__main__':
     try:
-        hello_world = rospy.ServiceProxy('hello_world', HelloWorld)
-        request = HelloWorldRequest()
-        response = hello_world(request)
-        return response.message
-    except rospy.ServiceException as e:
-        print("Service call failed:", e)
+        subscriber_node()
+    except rospy.ROSInterruptException:
+        pass
 
-if __name__ == "__main__":
-    rospy.init_node('hello_client')
-    result = hello_client()
-    print("Response from server:", result)
+
+
+
+# #!/usr/bin/env python
+
+# import rospy
+# from hello_world.srv import HelloWorld, HelloWorldRequest
+
+# def hello_client():
+#     rospy.wait_for_service('hello_world')
+#     try:
+#         hello_world = rospy.ServiceProxy('hello_world', HelloWorld)
+#         request = HelloWorldRequest()
+#         response = hello_world(request)
+#         return response.message
+#     except rospy.ServiceException as e:
+#         print("Service call failed:", e)
+
+# if __name__ == "__main__":
+#     rospy.init_node('hello_client')
+#     result = hello_client()
+#     print("Response from server:", result)
