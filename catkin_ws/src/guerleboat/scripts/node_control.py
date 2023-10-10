901dcf1e0d3bc8831cd5458f6b58a510740e2b5f
massath <theo.massa@ensta-bretagne.org>
2023-10-10 17:11:14 +0200

    eudq

--- catkin_ws/src/guerleboat/scripts/node_control.py
+++ catkin_ws/src/guerleboat/scripts/node_control.py
@@ -4,3 +4,19 @@
 import rospy
 import socket
 import numpy as np
+
+
+def control_node():
+    # Initialisation du noeud ROS
+    rospy.init_node('control')
+
+    rate = rospy.Rate(1)  # Par exemple, 1 message par seconde
+
+    while not rospy.is_shutdown():
+        rate.sleep()
+    
+if __name__ == '__main__':
+    try:
+        control_node()
+    except rospy.ROSInterruptException:
+        pass
