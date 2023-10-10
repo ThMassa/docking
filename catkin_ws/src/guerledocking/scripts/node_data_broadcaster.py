c22893409b3ef0fc27666db99d0ece5eb91c876c
BretzelDev <hugohofmann13@gmail.com>
2023-10-10 11:47:51 +0200

    working dock to UDP

--- catkin_ws/src/guerledocking/scripts/node_data_broadcaster.py
+++ catkin_ws/src/guerledocking/scripts/node_data_broadcaster.py
@@ -2,10 +2,11 @@
 # coding: latin-1
 
 import rospy
-from sbg_driver.msg import SbgEkfEuler
+from sbg_driver.msg import SbgEkfQuat
 from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import String
 import socket
+import numpy as np
 
 gps_data = None
 imu_data = None
@@ -14,11 +15,11 @@ def concatenate_data(gps_data, imu_angles):
     gps_header, gps_status = gps_data.header, gps_data.status
     lat, long = gps_data.latitude, gps_data.longitude
 
-    imu_timestamp, imu_status= imu_angles.time_stamp, imu_angles.status
-    angles = imu_angles.angle
-    angle_accuracy = imu_angles.accuracy
+    # imu_timestamp, imu_status= imu_angles.time_stamp, imu_angles.status
+    angles = imu_angles
+    # angle_accuracy = imu_angles.accuracy
 
-    data = "${};{},{};{},{},{}".format(imu_timestamp,lat,long,angles.x,angles.y,angles.z) # $timestamp;lat,long;x,y,z
+    data = "${},{};{},{},{}".format(lat,long,angles[0],angles[1],angles[2]) # $timestamp;lat,long;x,y,z
     
     return data
 
@@ -27,12 +28,31 @@ def unpack_data(data_string):
         print("##### ERROR, WRONG DATA FORMAT #####")
         return
     data_elements = data_string[1:].split(";")
-    timestamp, gps, angles = data_elements
+    gps, angles = data_elements
     timestamp = float(timestamp)
     x, y, z, = [float(val) for val in gps.split(",")]
     phi, theta, psi = [float(val) for val in angles.split(",")]
-    return timestamp, x, y, z, phi, theta, psi
+    return x, y, z, phi, theta, psi
 
+
+def euler_from_quaternion(quat):
+    """
+    Convert quaternion (w in last place) to euler roll, pitch, yaw.
+    quat = [x, y, z, w]
+    """
+    x = quat.x
+    y = quat.y
+    z = quat.z
+    w = quat.w
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+    sinp = 2 * (w * y - z * x)
+    pitch = np.arcsin(sinp)
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+    return roll, pitch, yaw
 
 def gps_callback(data):
     global gps_data
@@ -41,8 +61,7 @@ def gps_callback(data):
 
 def imu_callback(data):
     global imu_data
-    imu_data = data
-    print(imu_data.angle.x,imu_data.angle.y,imu_data.angle.z)
+    imu_data = euler_from_quaternion(data.quaternion)
 
 def broadcast_node():
     global gps_data,imu_data
@@ -50,11 +69,11 @@ def broadcast_node():
     rospy.init_node('data_broadcaster')
     
     rospy.Subscriber('/ublox/fix', NavSatFix, gps_callback)
-    rospy.Subscriber('/sbg/ekf_euler', SbgEkfEuler, imu_callback) ## ou (sbg/imu_data, SbgImuData
+    rospy.Subscriber('/sbg/ekf_quat', SbgEkfQuat, imu_callback) ## ou (sbg/imu_data, SbgImuData
 
     mon_publisher = rospy.Publisher('string_data', String, queue_size=10)
     # Configuration du socket UDP pour la communication avec le système distant
-    udp_ip = "192.168.0.11" #TODO mettre bonne adresse
+    udp_ip = "192.168.0.10" #TODO mettre bonne adresse
     udp_port = 12345  # Port UDP de destination sur le système distant
 
     # Création du socket UDP
