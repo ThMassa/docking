95ddf3c1c80029eaf175c05a89703f12cf6527c9
BretzelDev <hugohofmann13@gmail.com>
2024-02-08 15:43:58 +0100

    doc view satellite

--- Control/view_field.py
+++ Control/view_field.py
@@ -5,11 +5,15 @@ import mplleaflet
 from PIL import Image
 import requests
 
-# Function to load OpenStreetMap image as background
 def load_background_image(extent):
-    # Convert extent to pixel coordinates
-    xlim = [extent[0], extent[1]]
-    ylim = [extent[2], extent[3]]
+    """Récupère une image (carte) de la zone définie par extent
+
+    Args:
+        extent (list): domaine [lon_min, lat_min, lon_max, lat_max] de la zone considérée
+
+    Returns:
+        np.ndarray: image de la carte
+    """
 
     # Download OpenStreetMap image using requests
     url = "https://maps.geoapify.com/v1/staticmap?area=rect:{},{},{},{}&apiKey=43ddc05d66904824999d627ecc27f2c8"
@@ -20,13 +24,23 @@ def load_background_image(extent):
     if response.status_code == 200:
         # Open the image using Pillow
         image = Image.open(io.BytesIO(response.content))
-        return np.array(image), (xlim[0], xlim[1], ylim[0], ylim[1])
+        return np.array(image)
     else:
         print("Failed to download image:", response.status_code)
-        return None, None
+        return None
 
 
 def compute_view_rectangle(center, east_width, north_height):
+    """Calcule les limites [lon_min, lat_min, lon_max, lat_max] en fonction du centre [lon,lat] et de l'étendue en mètres sur les deux axes
+
+    Args:
+        center (list): Centre [lon,lat] de la zone voulue
+        east_width (float): étendue en mètres de par et d'autre du centre selon l'axe EST/OUEST
+        north_height (float): étendue en mètres de par et d'autre du centre selon l'axe NORD/SUD
+
+    Returns:
+        list : domaine [lon_min, lat_min, lon_max, lat_max]
+    """
     lon, lat = center
     R = 6480e3
     dlon, dlat = 180/np.pi * east_width/R, 180/np.pi * north_height/R
@@ -37,8 +51,7 @@ def compute_view_rectangle(center, east_width, north_height):
 
 
 
-def plot_map(map, longitude_bounds, latitude_bounds,
-             title, nro_fig=None):
+def plot_map(map, longitude_bounds, latitude_bounds, title, nro_fig=None):
     if nro_fig is None:
         ff = plt.figure()
     else:
@@ -57,17 +70,18 @@ def plot_map(map, longitude_bounds, latitude_bounds,
     #plt.pause(0.01)
     return gg
 
+## Coordonnées au milieu du lac
 lon_center = -3.0152798088714383
 lat_center = 48.198741654368305
 center = [lon_center, lat_center]
-offset = 0.01
 
-east_width = 100
-north_height = 100
-
-extent = [lon_center - offset, lat_center - offset, lon_center + offset, lat_center + offset]
+east_width = 100 #demi largeur de l'image en mètres
+north_height = 100 #demi hauteur de l'image en mètres
 extent = compute_view_rectangle(center, east_width=east_width, north_height=north_height)
 
-background_image, extent = load_background_image(extent)
+## Récupération de la carte de la zone 
+background_image = load_background_image(extent)
+
+## Affichage de la carte
 plot_map(background_image, [extent[0], extent[2]], [extent[1], extent[3]], "Guerlédan")
 plt.show()
