ee225231ae15b6b6e45e493224ce3e54ba233441
massath <theo.massa@ensta-bretagne.org>
2024-02-08 09:32:21 +0100

    ajout champ de vecteur sur visualisation

--- catkin_ws/src/guerleboat/scripts/node_visualization.py
+++ catkin_ws/src/guerleboat/scripts/node_visualization.py
@@ -19,6 +19,31 @@ c21, c22 = 10, 5  # constantes pour les champs de potentiels
 
 Xb,Xd,Xhat = None, None, None
 
+def f1(x1, x2):
+    phat = Xd[:2]
+    theta = Xd[-1,0]
+    n = np.array([[-sin(theta)], [cos(theta )]])
+    prod = n[0, 0]*(x1 - phat[0, 0]) + n[1, 0]*(x2 - phat[1, 0])
+    x10 = -c11 * n[0, 0] * prod - c12 * cos(theta)
+    x20 = -c11 * n[1, 0] * prod - c12 * sin(theta)
+    return x10, x20
+
+
+def f2(x1, x2):
+    phat = Xd[:2]
+    theta = Xd[-1,0]
+    x10 = c21*(x1 - phat[0, 0]) / ((x1 - phat[0, 0]) ** 2 + (x2 - phat[1, 0]) ** 2) ** (3 / 2) + c22 * cos(theta)
+    x20 = c21*(x2 - phat[1, 0]) / ((x1 - phat[0, 0]) ** 2 + (x2 - phat[1, 0]) ** 2) ** (3 / 2) + c22 * sin(theta)
+    return x10, x20
+
+def draw_field(ax,f,xmin,xmax,ymin,ymax,a):
+    Mx    = np.arange(xmin,xmax,a)
+    My    = np.arange(ymin,ymax,a)
+    X1,X2 = np.meshgrid(Mx,My)
+    VX,VY=f(X1,X2)
+    R=np.sqrt(VX**2+VY**2)
+    ax.quiver(Mx,My,VX/R,VY/R)
+
 def boat_pose_cb(msg):
     global Xb
     Xb = np.array([[msg.pose.position.x, 
@@ -51,30 +76,51 @@ def visualize_node():
     rospy.Subscriber('/docking/nav/dock_pose', PoseStamped, dock_pose_cb)
     rospy.Subscriber('/docking/nav/boat_kalman', PoseStamped, boat_kalman_cb)
 
-    f = 2
+    f = 5
     dt = 1./f
     rate = rospy.Rate(f)
     
     fig,ax = plt.subplots(1,1)
 
+    delta_draw = 50
+    value = 0
+    start = True
+
     while not rospy.is_shutdown():
         if Xb is not None and Xd is not None and Xhat is not None:
             ax.clear()
-            ax.set_xlim(Xd[0,0]-30,Xd[0,0]+30)
-            ax.set_ylim(Xd[1,0]-30,Xd[1,0]+30)
+            ax.set_xlim(Xd[0,0]-delta_draw,Xd[0,0]+delta_draw)
+            ax.set_ylim(Xd[1,0]-delta_draw,Xd[1,0]+delta_draw)
             ax.set_xlabel("East")
             ax.set_ylabel("North")
 
-            ax.scatter(Xb[0,0],Xb[1,0],label="Drone")
-            ax.quiver(Xb[0,0],Xb[1,0],cos(Xb[-1,0]),sin(Xb[-1,0]))
+            ax.scatter(Xb[0,0],Xb[1,0],label="Drone",color='green')
+            ax.quiver(Xb[0,0],Xb[1,0],cos(Xb[-1,0]),sin(Xb[-1,0]),color='green')
 
-            ax.scatter(Xd[0,0],Xd[1,0],label="dock")
-            ax.quiver(Xd[0,0],Xd[1,0],cos(Xd[-1,0]),sin(Xd[-1,0]))
+            ax.scatter(Xd[0,0],Xd[1,0],label="dock",color='red')
+            ax.quiver(Xd[0,0],Xd[1,0],cos(Xd[-1,0]),sin(Xd[-1,0]),color='red')
 
             ax.plot([Xd[0]-100*cos(Xd[-1,0]),Xd[0]+100*cos(Xd[-1,0])],[Xd[1]-100*sin(Xd[-1,0]),Xd[1]+100*sin(Xd[-1,0])])
 
-            ax.legend()
 
+            phat = Xd[:2]
+            theta = Xd[-1,0]
+
+            unit = np.array([[cos(theta)], [sin(theta)]])
+
+            if np.dot(unit.T,(Xb[:2] - phat)) < value and start:
+                draw_field(ax, f2, Xd[0,0]-delta_draw,Xd[0,0]+delta_draw, Xd[1,0]-delta_draw,Xd[1,0]+delta_draw, 5)
+                if value == 0:
+                    value = 3
+            else:
+                if start:
+                    start = False
+                draw_field(ax, f1, Xd[0,0]-delta_draw,Xd[0,0]+delta_draw, Xd[1,0]-delta_draw,Xd[1,0]+delta_draw, 5)
+                if np.dot(unit.T,(Xb[:2] - phat)) < -value:
+                    start = True
+
+
+            ax.legend()
             plt.pause(dt)
             
 
