67edc9d75572ac2e2b05bfb3b22f813e13f208c6
BretzelDev <hugohofmann13@gmail.com>
2024-02-08 15:48:01 +0100

    cleaner shit

--- Control/view_field.py
+++ Control/view_field.py
@@ -1,7 +1,6 @@
 import io
 import matplotlib.pyplot as plt
 import numpy as np
-import mplleaflet
 from PIL import Image
 import requests
 
@@ -42,7 +41,7 @@ def compute_view_rectangle(center, east_width, north_height):
         list : domaine [lon_min, lat_min, lon_max, lat_max]
     """
     lon, lat = center
-    R = 6480e3
+    R = 6378.137e3
     dlon, dlat = 180/np.pi * east_width/R, 180/np.pi * north_height/R
 
     rec = [lon - dlon, lat - dlat, lon + dlon, lat + dlat]
@@ -70,18 +69,20 @@ def plot_map(map, longitude_bounds, latitude_bounds, title, nro_fig=None):
     #plt.pause(0.01)
     return gg
 
-## Coordonnées au milieu du lac
-lon_center = -3.0152798088714383
-lat_center = 48.198741654368305
-center = [lon_center, lat_center]
 
-east_width = 100 #demi largeur de l'image en mètres
-north_height = 100 #demi hauteur de l'image en mètres
-extent = compute_view_rectangle(center, east_width=east_width, north_height=north_height)
+if __name__ == "__main__":
+    ## Coordonnées au milieu du lac
+    lon_center = -3.0152798088714383
+    lat_center = 48.198741654368305
+    center = [lon_center, lat_center]
 
-## Récupération de la carte de la zone 
-background_image = load_background_image(extent)
+    east_width = 100 #demi largeur de l'image en mètres
+    north_height = 100 #demi hauteur de l'image en mètres
+    extent = compute_view_rectangle(center, east_width=east_width, north_height=north_height)
 
-## Affichage de la carte
-plot_map(background_image, [extent[0], extent[2]], [extent[1], extent[3]], "Guerlédan")
-plt.show()
+    ## Récupération de la carte de la zone 
+    background_image = load_background_image(extent)
+
+    ## Affichage de la carte
+    plot_map(background_image, [extent[0], extent[2]], [extent[1], extent[3]], "Guerlédan")
+    plt.show()
