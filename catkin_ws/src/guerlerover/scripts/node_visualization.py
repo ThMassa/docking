ac5a839a43c40c6e5dada7a2f498f4cf17ba45f4
massath <theo.massa@ensta-bretagne.org>
2024-02-23 15:54:12 +0100

    ajout champ de vecteur sur visu + logs 23/02

--- catkin_ws/src/guerlerover/scripts/node_visualization.py
+++ catkin_ws/src/guerlerover/scripts/node_visualization.py
@@ -15,14 +15,16 @@ import matplotlib.pyplot as plt
 
 L, l = 1, 1  # taille Longueur largeur du dock
 marge = 1.5  # marge de securite, plus elle est elevee, plus le bateau s'arretera loin du dock et donc moins il aura de chance de se cogner contre le dock
-c11, c12 = 5, 200  # constantes pour les champs de potentiels
-c21, c22 = 5, 20  # constantes pour les champs de potentiels
+c11, c12 = 2.5, 1  # constantes pour les champs de potentiels
+c21, c22 = 2, 1  # constantes pour les champs de potentiels
 
 # Xb = np.zeros((5,1),dtype=np.float64)    #Pose du bateau (x,y,roll,pitch,yaw)
 # Xd = np.zeros((5,1),dtype=np.float64)    #Pose du dock   (x,y,roll,pitch,yaw)
 
 Xb,Xd,Xhat = None, None, None
 lambert = prj.Proj(init='EPSG:2154')
+hist_X = None
+
 
 def f1(x1, x2):
     phat = Xd[:2]
@@ -35,6 +37,7 @@ def f1(x1, x2):
 
 
 def f2(x1, x2):
+    # Xd[0,0],Xd[1,0] = lambert(Xd[0,0],Xd[1,0],inverse=True)
     phat = Xd[:2]
     theta = Xd[-1,0]
     x10 = c21*(x1 - phat[0, 0]) / ((x1 - phat[0, 0]) ** 2 + (x2 - phat[1, 0]) ** 2) ** (3 / 2) + c22 * cos(theta)
@@ -42,20 +45,33 @@ def f2(x1, x2):
     return x10, x20
 
 def draw_field(ax,f,xmin,xmax,ymin,ymax,a):
-    Mx    = np.arange(xmin,xmax,a)
-    My    = np.arange(ymin,ymax,a)
+    Mx    = np.linspace(xmin,xmax,a)
+    My    = np.linspace(ymin,ymax,a)
     X1,X2 = np.meshgrid(Mx,My)
     VX,VY=f(X1,X2)
+    
+    p1 = lambert(X1, X2, inverse=True)
+    p2 = lambert(X1 + VX, X2 + VY, inverse=True)
+    VX, VY = p2[0] - p1[0], p2[1] - p1[1]
+    
     R=np.sqrt(VX**2+VY**2)
+    Mx,My = lambert(Mx,My,inverse=True)
     ax.quiver(Mx,My,VX/R,VY/R)
 
 def rover_pose_cb(msg):
-    global Xb
+    global Xb, hist_X
     Xb = np.array([[msg.pose.position.x, 
                     msg.pose.position.y, 
                     msg.pose.orientation.x, 
                     msg.pose.orientation.y, 
                     msg.pose.orientation.z]]).T
+    lon,lat = lambert(Xb[0,0],Xb[1,0],inverse=True)
+    tmp = np.array([[lon,lat]]).T
+    if hist_X is None:
+        hist_X = tmp
+    
+    else :
+        hist_X = np.hstack((hist_X,tmp))
 
 def dock_pose_cb(msg):
     global Xd
@@ -72,7 +88,7 @@ def visualize_node():
     rospy.Subscriber('/docking/nav/rover_pose', PoseStamped, rover_pose_cb)
     rospy.Subscriber('/docking/nav/dock_pose', PoseStamped, dock_pose_cb)
 
-    f = 1
+    f = 10
     dt = 1./f
     rate = rospy.Rate(f)
     
@@ -92,65 +108,58 @@ def visualize_node():
     
     fig,ax = plt.subplots(1,1)
 
-    delta_draw = 50
     value = 0
     start = True
-
+    xmin,ymin = lambert(longitude_bounds[0],latitude_bounds[0])
+    xmax,ymax = lambert(longitude_bounds[1],latitude_bounds[1])
+        
     while not rospy.is_shutdown():
         if Xb is not None and Xd is not None:
-            # print("plot")
+            # Xb[0,0],Xb[1,0] = lambert(Xb[0,0],Xb[1,0],inverse=True)
+            # Xd[0,0],Xd[1,0] = lambert(Xd[0,0],Xd[1,0],inverse=True)
+            
             ax.clear()
-            # ax.set_xlim(Xd[0,0]-delta_draw,Xd[0,0]+delta_draw)
-            # ax.set_ylim(Xd[1,0]-delta_draw,Xd[1,0]+delta_draw)
-            # ax.set_xlabel("East")
-            # ax.set_ylabel("North")
             
             ax.imshow(background_image, extent=[longitude_bounds[0], longitude_bounds[1],
-                                   latitude_bounds[0], latitude_bounds[1]],
-                           aspect="equal")
+                                latitude_bounds[0], latitude_bounds[1]],
+                        aspect="equal")
 
             ax.set_xlim(longitude_bounds[0], longitude_bounds[1])
             ax.set_ylim(latitude_bounds[0], latitude_bounds[1])
             ax.set_xlabel("longitude (deg)")
             ax.set_ylabel("latitude (deg)")
             
-            # phat = Xd[:2]
-            # theta = Xd[-1,0]
+            phat = Xd[:2]
+            theta = Xd[-1,0]
 
-            # unit = np.array([[cos(theta)], [sin(theta)]])
+            unit = np.array([[cos(theta)], [sin(theta)]])
+            a = 20
+            if np.dot(unit.T,(Xb[:2] - phat)) < value and start:
+                draw_field(ax, f2, xmin,xmax,ymin,ymax, a)
+                if value == 0:
+                    value = 2*L
+            else:
+                if start:
+                    start = False
+                draw_field(ax, f1, xmin,xmax,ymin,ymax, a)
+                if np.dot(unit.T,(Xb[:2] - phat)) < -value/2:
+                    start = True
 
-            # if np.dot(unit.T,(Xb[:2] - phat)) < value and start:
-            #     draw_field(ax, f2, Xd[0,0]-delta_draw,Xd[0,0]+delta_draw, Xd[1,0]-delta_draw,Xd[1,0]+delta_draw, 5)
-            #     if value == 0:
-            #         value = 3*L
-            # else:
-            #     if start:
-            #         start = False
-            #     draw_field(ax, f1, Xd[0,0]-delta_draw,Xd[0,0]+delta_draw, Xd[1,0]-delta_draw,Xd[1,0]+delta_draw, 5)
-            #     if np.dot(unit.T,(Xb[:2] - phat)) < -value/3:
-            #         start = True
-            
-            
             Xb[0,0],Xb[1,0] = lambert(Xb[0,0],Xb[1,0],inverse=True)
-            # print(Xd[:2])
             Xd[0,0],Xd[1,0] = lambert(Xd[0,0],Xd[1,0],inverse=True)
-            # print(Xd[:2].flatten())
-
-            ax.scatter(Xb[0,0],Xb[1,0],label="Drone",color='green')
-            ax.quiver(Xb[0,0],Xb[1,0],cos(Xb[-1,0]),sin(Xb[-1,0]),color='green')
-
+                        
             ax.scatter(Xd[0,0],Xd[1,0],label="dock",color='red')
             ax.quiver(Xd[0,0],Xd[1,0],cos(Xd[-1,0]),sin(Xd[-1,0]),color='red')
 
+            # ax.scatter(Xb[0,0],Xb[1,0],label="Drone",color='green')
+            # ax.quiver(Xb[0,0],Xb[1,0],cos(Xb[-1,0]),sin(Xb[-1,0]),color='green')
             # ax.plot([Xd[0]-100*cos(Xd[-1,0]),Xd[0]+100*cos(Xd[-1,0])],[Xd[1]-100*sin(Xd[-1,0]),Xd[1]+100*sin(Xd[-1,0])])
 
-
-
-
+            ax.plot(hist_X[0,:],hist_X[1,:],label="Rover",color="green")
+            ax.quiver(Xb[0,0],Xb[1,0],cos(Xb[-1,0]),sin(Xb[-1,0]),color='green')
             ax.legend()
-            plt.pause(dt)
-            
-
+            plt.pause(10*dt)             
+                
         rate.sleep()
     
 if __name__ == '__main__':
